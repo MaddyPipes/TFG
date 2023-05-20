38aca4ac9725fade03c48af7893dfcd4cee035b4
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-20 12:20:13 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@ $gestor = new Jugador();
 //Nos conectamos a la Bd
 $conexPDO = Utils::conectar();
 //Recolectamos los datos de los jugadores
-$datosJugador = $gestor->getJugador($conexPDO, true, $pag);
+$datosJugador = $gestor->getJugador($conexPDO, true, 1);
 // phpinfo();
 
 include("./views/main_page.php");
