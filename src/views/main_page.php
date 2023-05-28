0f9a5c2663e45757781cdf5e5d0e0a1bfe0170de
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-28 18:31:10 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -35,7 +35,7 @@
                         <a href="" type="button" role="button" tabindex="1" data-bs-toggle="modal" data-bs-target="#modalUsuario"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                            </svg><?php if(isset($_SESSION['user_name'])){print $_SESSION['user_name'];} else {print "Usuario";}?></a>
+                            </svg><?php if(isset($_SESSION['user_name'])){print $_SESSION['user_name'];} else {print "Usuarie";}?></a>
                         <div class="modal fade" id="modalUsuario" tabindex="-1" aria-labelledby="modalUsuarioHeader" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content" id="cuerpoModalUsuario"></div>
