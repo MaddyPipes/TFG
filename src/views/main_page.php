d10d2cc1818b6d3618c91b8fa9a57b32aa277f95
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-31 21:10:59 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -31,7 +31,7 @@
             <div class="col-12 col-md-8 menu margin-top_8--sm">
                 <ul>
                     <li>
-                        <a href="#offcanvasExample" type="button" role="button" tabindex="1" data-bs-toggle="modal" data-bs-target="#modalUsuario"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje">
+                        <a href="" type="button" role="button" tabindex="1" data-bs-toggle="modal" data-bs-target="#modalUsuario"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                             </svg><?php if (isset($_SESSION['user_name'])) {
@@ -41,7 +41,7 @@
                                     } ?></a>
                     </li>
                     <li>
-                        <a href="" role="button" tabindex="1"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+                        <a href="#offcanvasPersonaje" role="button" tabindex="1"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
                             </svg>Personaje</a>
