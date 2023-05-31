260f95240fe17b66157db86122ddc2f58b49c623
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-31 21:34:42 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -70,23 +70,12 @@
 
     <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasPersonaje" aria-labelledby="offcanvasPersonaje">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Personajes</h5>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
-        <div class="offcanvas-body">
-            <div>
-                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
-            </div>
-            <div class="dropdown mt-3">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
-                    Dropdown button
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-            </div>
+        <div class="offcanvas-body d-flex justify-content-around">
+            <button class="btn btn-primary">Crear Nuevo Personaje</button>
+            <button class="btn btn-danger">Salir</button>
         </div>
     </div>
 
