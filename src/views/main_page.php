5ee60ee6a092a224f5f9b5d40172f3dac275d9b9
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-31 21:08:46 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -31,10 +31,14 @@
             <div class="col-12 col-md-8 menu margin-top_8--sm">
                 <ul>
                     <li>
-                        <a href="" type="button" role="button" tabindex="1" data-bs-toggle="modal" data-bs-target="#modalUsuario"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <a href="#offcanvasExample" type="button" role="button" tabindex="1" data-bs-toggle="modal" data-bs-target="#modalUsuario"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
-                            </svg><?php if(isset($_SESSION['user_name'])){print $_SESSION['user_name'];} else {print "Usuario";}?></a>
+                            </svg><?php if (isset($_SESSION['user_name'])) {
+                                        print $_SESSION['user_name'];
+                                    } else {
+                                        print "Usuario";
+                                    } ?></a>
                     </li>
                     <li>
                         <a href="" role="button" tabindex="1"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
@@ -51,6 +55,29 @@
             </div>
         </div>
     </nav>
+
+    <!-- Offcanvas -->
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasPersonaje" aria-labelledby="offcanvasPersonaje">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div>
+                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+            </div>
+            <div class="dropdown mt-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                    Dropdown button
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
     <script src="../scripts/includes.js"></script>
 
     <!-- Peticiones AJAX -->
