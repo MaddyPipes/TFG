3e5a8ca8b11e2171cf0a40926c6745e2c4d268c0
Maddy_Pipes <alexisthinking@gmail.com>
2023-05-31 21:13:14 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -41,7 +41,7 @@
                                     } ?></a>
                     </li>
                     <li>
-                        <a href="#offcanvasPersonaje" role="button" tabindex="1"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje">
+                        <a href="#offcanvasPersonaje" role="button" tabindex="1" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
                             </svg>Personaje</a>
