215e784037c48696ad861748c826c8eb24dc3ca5
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-04 18:30:42 +0200

    Update crearPersonajeDYD.php

--- src/views/crearPersonajeDYD.php
+++ src/views/crearPersonajeDYD.php
@@ -9,6 +9,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/stylesheet.css">
+    <style>
+    input:focus{
+        box-shadow: none;
+    }
+    input:checked{
+        background-color: red;
+        border-color: red;
+    }
+    </style>
 </head>
 
 <body style="background-image:url(../rsc/wp2227193-dungeons-dragons-wallpapers.jpg); background-position:center; background-size:cover; background-repeat:no-repeat;">
