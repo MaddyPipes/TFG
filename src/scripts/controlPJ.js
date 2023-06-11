31e38e29f23bdd51a256293624967cba9855d300
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-11 19:29:53 +0200

    Update controlPJ.js

--- src/scripts/controlPJ.js
+++ src/scripts/controlPJ.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
                 if (response === "success") {
                     alert("El elemento se ha eliminado correctamente.");
                 } else {
-                    alert("Hubo un error al eliminar el elemento."+personajeID);
+                    alert("Hubo un error al eliminar el elemento."+personajeID+response);
                 }
             },
             error: function () {
