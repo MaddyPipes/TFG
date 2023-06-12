60a94690551837f538847b1b798a7b42758b718b
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-12 21:08:23 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -223,11 +223,11 @@
 
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvaSalas" aria-labelledby="offcanvaSalasLabel">
         <div class="offcanvas-header">
-            <h5 id="offcanvaSalasLabel">Offcanvas right</h5>
+            <h5 id="offcanvaSalasLabel">POLLAS</h5>
             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            ...
+            
         </div>
     </div>
 
