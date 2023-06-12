d1e9ec32f2ea21a36fab5c3bffe9c02b7c15acfb
Alejandro Ceballos Cuevas <alexisthinking@gmail.com>
2023-06-12 23:52:35 +0200

    Update vistaDYD.php

--- src/views/vistaDYD.php
+++ src/views/vistaDYD.php
@@ -43,9 +43,9 @@
                 <div class="align-items-center col-lg-9 col-sm-9 d-flex flex-column page" id="page1">
 
                     <div class="row">
-                        <div class="col-4 d-flex justify-content-center stat" style="background-image: url(../rsc/caIcon.png);">
+                        <div class="col-4 d-flex flex-column align-items-center justify-content-center stat" style="background-image: url(../rsc/caIcon.png);">
                             <div>CA</div>
-                            <div id="ca">16</div>
+                            <div id="ca" class="statNumber">16</div>
                         </div>
                         <div class="col-4">
 
