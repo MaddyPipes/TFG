ab5d1c449437cc403161f3f01575bbbd483c908c
Alejandro Ceballos Cuevas <alexisthinking@gmail.com>
2023-06-12 23:06:52 +0200

    Update vistaDYD.php

--- src/views/vistaDYD.php
+++ src/views/vistaDYD.php
@@ -1,37 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>What The Dice! | D&D</title>
+    <title>WHAT THE DICE || DYD</title>
     <link rel="apple-touch-icon" sizes="180x180" href="../rsc/favicon_io/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../rsc/favicon_io/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="../rsc/favicon_io/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../styles/stylesheet.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <style>
+        input:focus {
+            box-shadow: none !important;
+            border-color: red !important;
+        }
+
+        input:checked {
+            background-color: red !important;
+            border-color: red !important;
+        }
+    </style>
 </head>
 
-<body>
+<body style="background-image:url(../rsc/wp2227193-dungeons-dragons-wallpapers.jpg); background-position:center; background-size:cover; background-repeat:no-repeat;">
 
-    <header class="container margin-top_48">
-        <div class="row justify-content-center margin-bottom_16 margin-top_16">
-            <div class="col-12 banner">
-                <img src="../rsc/banner.png" alt="banner principal" class="banner_img">
+    <?php
+    //Definimos el destino de el formulario
+    $url_destino = "../controller/registroUsuarioController.php";
+    ?>
+
+    <form method="POST" action="../controller/crearPersonajeModelo.php" id="registroUsuario">
+
+        <div class="container margin-top_48 margin-bottom_48 border border-2" style="background-color: #353535;">
+
+            <div class="row justify-content-center">
+
+                <div class="col-12 d-flex justify-content-center"><img src="../rsc/whited20.png" alt=""></div>
+
+                <div class="col-12 d-flex justify-content-center"><img class="img-fluid col-5" src="../rsc/DnD-Logo.png" alt=""></div>
+
+                <div class="align-items-center col-lg-9 col-sm-9 d-flex flex-column page" id="page1">
+
+
+                </div>
             </div>
         </div>
-    </header>
-    <main class="container d-flex align-items-center">
-        <div class="row">
-            <div class="col-12">
+    </form>
 
-            </div>
-        </div>
-    </main>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- jQuery Validation Plugin -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/jquery.validate.min.js"></script>
+
+    <!-- jQuery Validation Additional Methods -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.3/additional-methods.min.js"></script>
+
+    </script>
 
 </body>
 
