4091aafee75c8dd3c85affdede2d3ae2c3365c56
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-13 08:53:05 +0200

    Update vistaDYD.php

--- src/views/vistaDYD.php
+++ src/views/vistaDYD.php
@@ -2,6 +2,9 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WHAT THE DICE || DYD</title>
     <link rel="apple-touch-icon" sizes="180x180" href="../rsc/favicon_io/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../rsc/favicon_io/favicon-32x32.png">
@@ -26,38 +29,38 @@
 
 <body style="background-image:url(../rsc/wp2227193-dungeons-dragons-wallpapers.jpg); background-position:center; background-size:cover; background-repeat:no-repeat;">
 
-        <div class="container margin-top_48 margin-bottom_48 border border-2" style="background-color: #353535;">
+    <div class="container margin-top_48 margin-bottom_48 border border-2" style="background-color: #353535;">
 
-            <div class="row justify-content-center">
+        <div class="row justify-content-center">
 
-                <div class="col-7 d-flex justify-content-center"><img src="../rsc/whited20.png" alt="logoD20" style="max-width: 5em;"></div>
+            <div class="col-7 d-flex justify-content-center"><img src="../rsc/whited20.png" alt="logoD20" style="max-width: 5em;"></div>
 
-                <div class="col-7 d-flex justify-content-center"><img class="img-fluid col-5" src="../rsc/DnD-Logo.png" alt="logoDYD"></div>
+            <div class="col-7 d-flex justify-content-center"><img class="img-fluid col-5" src="../rsc/DnD-Logo.png" alt="logoDYD"></div>
 
-                <div class="align-items-center col-9">
+            <div class="align-items-center col-9">
 
-                    <div class="row align-items-center justify-content-center">
-                        <div class="col-4 stat" style="background-image: url(../rsc/pgIcon.png);">
-                            <div>PG</div>
-                            <div class="d-flex align-items-center justify-content-center">
-                                <input type="text" value="" id="actualHP"> / <span id="hp" class="statNumber"></span>
-                            </div>
-                        </div>
-                        <div class="col-4 stat" style="background-image: url(../rsc/inspiIcon.png);">
-                            <div>INS</div>
-                            <div id="insp">1</div>
-                        </div>
-                        <div class="col-4 stat" style="background-image: url(../rsc/caIcon.png);">
-                            <div>CA</div>
-                            <div class="d-flex align-items-center justify-content-center">
-                                <span id="ca" class="statNumber"></span>
-                            </div>
+                <div class="row align-items-center justify-content-center">
+                    <div class="col-4 stat" style="background-image: url(../rsc/pgIcon.png);">
+                        <div>PG</div>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <input type="text" value="" id="actualHP"> / <span id="hp" class="statNumber"></span>
                         </div>
                     </div>
-
+                    <div class="col-4 stat" style="background-image: url(../rsc/inspiIcon.png);">
+                        <div>INS</div>
+                        <div id="insp">1</div>
+                    </div>
+                    <div class="col-4 stat" style="background-image: url(../rsc/caIcon.png);">
+                        <div>CA</div>
+                        <div class="d-flex align-items-center justify-content-center">
+                            <span id="ca" class="statNumber"></span>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
+    </div>
 
     <script src="../scripts/controlDYD.js"></script>
 
