0311b96022faa924941f5edfe417065b2f71b272
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-13 08:54:18 +0200

    Update crearPersonajeDYD.php

--- src/views/crearPersonajeDYD.php
+++ src/views/crearPersonajeDYD.php
@@ -2,6 +2,9 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WHAT THE DICE || Creación Personaje</title>
     <link rel="apple-touch-icon" sizes="180x180" href="../rsc/favicon_io/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="../rsc/favicon_io/favicon-32x32.png">
