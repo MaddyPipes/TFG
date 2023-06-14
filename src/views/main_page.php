287f2eb6af79aa0bc16b77c81434aab468354cf1
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-14 22:37:13 +0200

    Update main_page.php

--- src/views/main_page.php
+++ src/views/main_page.php
@@ -25,15 +25,15 @@
         </div>
     </header>
     <nav class="container">
-        <div class="row justify-content-center">
-            <div class="col-6">
-                <a href="#offcanvasPersonaje" role="button" tabindex="1" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
+        <div class="row justify-content-around">
+            <div class="col-5 menu">
+                <a href="#offcanvasPersonaje" role="button" tabindex="1" data-bs-toggle="offcanvas" aria-controls="offcanvasPersonaje" class="text-light text-decoration-none d-flex flex-column"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-square" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1v-1c0-1-1-4-6-4s-6 3-6 4v1a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12z" />
                     </svg>Personaje</a>
             </div>
-            <div class="col-6">
-                <a href="#offcanvaSalas" role="button" tabindex="1" data-bs-toggle="offcanvas" aria-controls="offcanvaSalas"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+            <div class="col-5 menu">
+                <a href="#offcanvaSalas" role="button" tabindex="1" data-bs-toggle="offcanvas" aria-controls="offcanvaSalas" class="text-light text-decoration-none d-flex flex-column"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-door-open-fill" viewBox="0 0 16 16">
                         <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z" />
                     </svg>Salas</a>
             </div>
