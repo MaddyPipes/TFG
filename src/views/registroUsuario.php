46d89f5c699090fb9d3dabc241b54279e97a28cc
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-14 22:26:20 +0200

    Update registroUsuario.php

--- src/views/registroUsuario.php
+++ src/views/registroUsuario.php
@@ -8,7 +8,6 @@
     <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../styles/stylesheet.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <style>
         .error.fail-alert {
             border: 2px solid red;
@@ -147,7 +146,7 @@
 
                 <div class="col-12 d-flex justify-content-center"><img src="../rsc/whited20.png" alt=""></div>
 
-                <div class="col-lg-9 col-sm-9">
+                <div class="col-lg-9 col-sm-9 text-center">
 
                     <!-- Margenes con mb mr ml mt -sm-distancia-->
                     <!-- Misma linea -->
