67fab7e0b480765bc0a65a93710d3ec69b5b5054
Maddy_Pipes <alexisthinking@gmail.com>
2023-06-14 17:42:19 +0200

    f

--- src/views/vistaDYD.php
+++ src/views/vistaDYD.php
@@ -32,7 +32,7 @@
 
     <div class="container margin-top_48 margin-bottom_48 border border-2" style="background-color: #353535;">
 
-        <div class="row justify-content-center"  id="dice-box">
+        <div class="row justify-content-center">
 
             <div class="col-7 d-flex justify-content-center"><img class="img-fluid col-5" src="../rsc/DnD-Logo.png" alt="logoDYD"></div>
 
@@ -448,7 +448,7 @@
             </div>
         </div>
     </div>
-
+    <div id="dice-box"></div>
     <script type="module" src="../scripts/controlDYD.mjs" crossorigin="anonymous"></script>
 
 </body>
